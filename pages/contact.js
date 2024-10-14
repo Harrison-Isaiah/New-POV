d898d1f576fe1ee4a8e18c6556fe17a7975c47ed
Harrison Isaiah <97739869+Harrison-Isaiah@users.noreply.github.com>
2024-10-14 17:11:40 +0500

    Add files via upload

---
+++ pages/contact.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Link from "next/link";
+import Brand from "@/components/ui/Brand";
+import Button from "@/components/ui/Button";
+import Input from "@/components/ui/Input";
+import { GoogleIcon } from "@/components/Icons";
+import Contact from "@/components/ui/Contact/Contact";
+
+export default function Login() {
+  return (
+    <>
+          <Contact/>
+    </>
+  );
+}
